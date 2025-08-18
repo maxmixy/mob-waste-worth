3c509daa08156805e0438f6113bd5df5a19ba2dc
maxmixy <163897493+maxmixy@users.noreply.github.com>
2025-08-19 02:16:24 +0800

    fixes

--- w2w/lib/firebaseConfig.ts
+++ w2w/lib/firebaseConfig.ts
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +23,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
+// React Native auth persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Firestore and Storage
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
